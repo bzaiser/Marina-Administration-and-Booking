298cbdd1cd1076cc97c8c2827f3ca0b629c09133
Bernd Zaiser <bernd@zaisers.de>
2026-05-08 13:34:12 +0200

    Removed redundant migrate/collectstatic from update script to prevent DB lock issues.

--- docker-update.sh
+++ docker-update.sh
@@ -3,6 +3,4 @@
 git pull
 mkdir -p data
 docker-compose up -d --build
-docker-compose exec web python manage.py migrate
-docker-compose exec web python manage.py collectstatic --noinput
-echo "Marina updated and restarted."
+echo "Marina updated and restarting. Entrypoint will handle migrations."
