17c774312a4057e8ffe0f881c1748db85e9abc64
Bernd Zaiser <bernd@zaisers.de>
2026-05-08 13:04:35 +0200

    Added mkdir -p data to docker-update.sh for completeness.

--- docker-update.sh
+++ docker-update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Update Marina in Docker
 git pull
+mkdir -p data
 docker-compose up -d --build
 docker-compose exec web python manage.py migrate
 docker-compose exec web python manage.py collectstatic --noinput
