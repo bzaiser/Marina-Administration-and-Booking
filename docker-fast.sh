61b168e5c6337ea72df8c7f34de80449db846e80
Bernd Zaiser <bernd@zaisers.de>
2026-05-13 11:18:10 +0200

    Update docker-fast.sh

--- docker-fast.sh
+++ docker-fast.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Start Marina in Docker FAST (no build check)
-mkdir -p data
-docker-compose up -d
+
 echo "Marina is starting FAST on port $(grep WEB_PORT .env | cut -d '=' -f2 || echo 8004)"
+echo "Updating containers..."
+docker compose --env-file .env up -d
+docker compose --env-file .env restart web
