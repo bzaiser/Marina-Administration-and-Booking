e17774e2ea1f78ab37d4d8e95c04de73f93531e2
Bernd Zaiser <bernd@zaisers.de>
2026-05-13 11:32:21 +0200

    Include git pull in docker-fast.sh

--- docker-fast.sh
+++ docker-fast.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Start Marina in Docker FAST (no build check)
 
+echo "Pulling latest changes from Git..."
+git pull origin main
+
 echo "Marina is starting FAST on port $(grep WEB_PORT .env | cut -d '=' -f2 || echo 8004)"
 echo "Updating containers..."
 docker compose --env-file .env up -d
